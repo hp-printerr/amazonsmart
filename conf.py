4f24e39cf9213314b200261417461cd53b0c20fb
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-06-28 23:54:13 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ project = 'Amazon Smart'
 
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+#release = '1.0.0'
 
 html_theme_options = {
     'collapse_navigation': True,   # Collapse nav by default
