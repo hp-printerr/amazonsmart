b11673d14390673ec5616f69898136cf83ab5394
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-06-28 23:06:33 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -22,25 +22,16 @@ project = 'Amazon Smart'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
+html_theme_options = {
+    'collapse_navigation': True,   # Collapse nav by default
+    'sticky_navigation': False,    # Disable sticky sidebar
+    'navigation_depth': 1,          # Show only top level items
+    'includehidden': False,
+    'titles_only': True
+}
 
-# Add any Sphinx extension module names here, as strings. 
-# These extensions are optional but useful for Read the Docs.
 
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.
-# Use the Read the Docs theme for better compatibility with Read the Docs.
-# html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets)
 # here, relative to this directory. These files are copied after the built-in
